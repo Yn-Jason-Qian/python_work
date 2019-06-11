87396eb60462607a53bdbdcee6c123dec96b6850
qian.yang <qian.yang@chinaredstar.com>
2019-06-11 14:15:30 +0800

    update

--- part12/alien_invasion/settings.py
+++ part12/alien_invasion/settings.py
@@ -15,4 +15,8 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_speed_factor = 1
-
+        
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
