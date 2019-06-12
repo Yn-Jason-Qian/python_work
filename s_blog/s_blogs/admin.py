968023a987dbcf53d53d09a3b8dc31b528f2588f
qian.yang <qian.yang@chinaredstar.com>
2019-06-12 18:21:50 +0800

    add s_blog

---
+++ s_blog/s_blogs/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from s_blogs.models import Topic, Entry
+# Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Entry)
---
+++ s_blog/s_blogs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SBlogsConfig(AppConfig):
+    name = 's_blogs'
